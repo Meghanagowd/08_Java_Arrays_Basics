c3375c2c01f4851a2779e77afd00941b054ba2fa
Meghanagowd <gowdameghana256@gmail.com>
2026-02-24 21:42:17 +0530

    Day 8: Solved Largest Element in Array (GFG)
    - Used linear traversal
    - Maintained running maximum

---
+++ LargestElement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LargestElement {
+
+    public static int largest(int[] arr) {
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Largest element: " + largest(arr));
+
+        sc.close();
+    }
+}
